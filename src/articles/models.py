3267b6593e7f9993b825be12846a7a952bc45445
Kirill Ilenkov <kirillilenkov974@gmail.com>
2025-05-19 17:44:22 +0400

    Added new UserSchema model

--- src/articles/models.py
+++ src/articles/models.py
@@ -10,3 +10,12 @@ class ArticleSchema(BaseModel):
     author: Mapped[str] = mapped_column()
     title: Mapped[str] = mapped_column()
     content: Mapped[str] = mapped_column()
+
+class UserSchema(BaseModel):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    username: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column()
+    password: Mapped[str] = mapped_column()

7299d484c979e787f81253677f4e1ee253435113
Kirill Ilenkov <kirillilenkov974@gmail.com>
2025-05-22 18:24:22 +0400

    Added simple template to show articles

--- src/articles/service.py
+++ src/articles/service.py
@@ -1,18 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Response
+from fastapi.responses import HTMLResponse
 from typing import List, Dict, Union
 
 from src.articles.schemas import ArticleModel
 from src.dependencies import SessionDep
 from src.articles.utils import get_list_articles, add_article_db
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 
+# router.mount()
+templates = Jinja2Templates(directory="templates")
 
-@router.get('/blog')
-async def get_articles(Session: SessionDep) -> List[ArticleModel]:
+
+@router.get('/blog', response_class=HTMLResponse)
+async def get_articles(request: Request, Session: SessionDep):
     articles = await get_list_articles(Session)
 
-    return articles
+    # return articles
+
+    return templates.TemplateResponse(
+        request=request,
+        name='blog.html',
+        context={'articles': articles}
+    )
 
 
 @router.post('/blog')

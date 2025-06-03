88abc6eff155003ba9c3c35b715f3fb4061d0bfb
Kirill Ilenkov <kirillilenkov974@gmail.com>
2025-06-03 19:28:54 +0400

    Added new fields to ArticleSchema

--- src/articles/schemas.py
+++ src/articles/schemas.py
@@ -1,7 +1,31 @@
-from pydantic import BaseModel
+from datetime import datetime
+
+from sqlalchemy import DateTime, Nullable
+from sqlalchemy.orm import Mapped, mapped_column
+from src.database import BaseModel
 
 
-class ArticleModel(BaseModel):
-    author: str
-    title: str
-    content: str
+class ArticleSchema(BaseModel):
+    __tablename__ = "articles"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    author: Mapped[str] = mapped_column()
+    title: Mapped[str] = mapped_column()
+    content: Mapped[str] = mapped_column()
+
+    release_date: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.now, onupdate=datetime.now
+    )
+
+    secret_info: Mapped[str] = mapped_column(nullable=True)
+
+
+class UserSchema(BaseModel):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    username: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column()
+    password: Mapped[str] = mapped_column()

0184d51c38f3111386653a1d11884ee5f4682037
Kirill Ilenkov <kirillilenkov974@gmail.com>
2025-06-06 17:58:37 +0400

    Added delete and uptade endpoint for ArticleSchema

--- src/articles/service.py
+++ src/articles/service.py
@@ -1,13 +1,20 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import HTMLResponse
 
-from src.articles.models import ArticleCreate, ArticlePublic
+from src.articles.models import ArticleCreate, ArticlePublic, ArticleUpdate
 from src.articles.schemas import ArticleSchema
 from src.dependencies import SessionDep
-from src.articles.utils import get_list_articles, add_article_db
+from src.articles.utils import (
+    get_list_articles,
+    add_article_db,
+    delete_article_db,
+    update_article_db,
+)
 from fastapi.templating import Jinja2Templates
 
-router = APIRouter()
+router = APIRouter(
+    tags=["Articles 📰"],
+)
 
 # router.mount()
 templates = Jinja2Templates(directory="templates")
@@ -22,7 +29,7 @@ async def get_articles(request: Request, Session: SessionDep, page_id: int = 1):
     )
 
 
-@router.post("/blog", response_model=ArticlePublic)
+@router.post("/article", response_model=ArticlePublic)
 async def add_article(
     Session: SessionDep, article_data: ArticleCreate
 ) -> ArticleSchema:
@@ -30,5 +37,31 @@ async def add_article(
         article = await add_article_db(Session, article_data)
 
         return article
+    except Exception as e:
+        raise e
+
+
+@router.delete("/article")
+async def delete_article(Session: SessionDep, article_id: int):
+
+    try:
+        await delete_article_db(Session=Session, article_id=article_id)
+
+        return {"ok": True}
+    except Exception as e:
+        raise e
+
+
+@router.patch("/article")
+async def update_article(
+    Session: SessionDep, article_id: int, new_article_data: ArticleUpdate
+):
+    try:
+        article_db = await update_article_db(
+            Session=Session, article_id=article_id, new_article_data=new_article_data
+        )
+
+        return article_db
+
     except Exception as e:
         raise e
--- src/articles/utils.py
+++ src/articles/utils.py
@@ -1,8 +1,8 @@
 from src.articles.schemas import ArticleSchema
-from src.articles.models import ArticleCreate
+from src.articles.models import ArticleCreate, ArticleUpdate
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import Select
-
+from fastapi import Depends, HTTPException
 
 async def get_list_articles(Session: AsyncSession, limit=5, page_id=1):
     query = Select(ArticleSchema).offset((page_id - 1) * limit).limit(limit)
@@ -29,3 +29,30 @@ async def add_article_db(
     await Session.commit()
 
     return article
+
+async def delete_article_db(Session: AsyncSession, article_id: int):
+    article_db = await Session.get(ArticleSchema, article_id)
+
+    if not article_db:
+        raise HTTPException(status_code=404, detail="Article not found")
+
+    await Session.delete(article_db)
+
+    await Session.flush()
+    await Session.commit()
+
+async def update_article_db(Session: AsyncSession, article_id: int, new_article_data: ArticleUpdate):
+    article_db = await Session.get(ArticleSchema, article_id)
+
+    if not article_db:
+        raise HTTPException(status_code=404, detail="Article not found")
+
+    article_data = new_article_data.model_dump(exclude_unset=True, exclude_none=True).items()
+
+    for key, value in article_data:
+        setattr(article_db, key, value)
+
+    await Session.flush()
+    await Session.commit()
+
+    return article_db
